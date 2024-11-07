403a8fde0c8a6b1d0d67175f3516c2fa45828bfd
Francisco Monroy <83427525+fmonroy75@users.noreply.github.com>
2024-11-06 20:38:50 -0500

    xxx

--- SmartigerDev/assets/js/scripts.js
+++ SmartigerDev/assets/js/scripts.js
@@ -36,19 +36,6 @@ $('.carousel-control-prev, .carousel-control-next').hover(
     }
 );
 
-//fadein
-$(document).ready(function(){
-    $(window).scroll(function(){
-        $('.fadeInUp').each(function(i){
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            if(bottom_of_window > bottom_of_object){
-                $(this).animate({'opacity':'1', 'margin-top':'0px'}, 600);
-            }
-        });
-    });
-});
 
 
 //suavizado enlaces
@@ -60,3 +47,5 @@ $(document).ready(function(){
         }, 800);
     });
 });
+
+
